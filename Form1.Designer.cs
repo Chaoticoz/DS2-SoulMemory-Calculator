eed2fc97d92cd8ab9c294a40b8202e7f127eddcc
thomas <thomas.klemenc@gmail.com>
2020-04-30 20:20:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SoulMemory_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.chkRing = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(6, 25);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(27, 13);
+            this.lblMin.TabIndex = 2;
+            this.lblMin.Text = "Min:";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(6, 48);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(30, 13);
+            this.lblMax.TabIndex = 3;
+            this.lblMax.Text = "Max:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(77, 19);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 4;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // chkRing
+            // 
+            this.chkRing.AutoSize = true;
+            this.chkRing.Location = new System.Drawing.Point(9, 45);
+            this.chkRing.Name = "chkRing";
+            this.chkRing.Size = new System.Drawing.Size(128, 17);
+            this.chkRing.TabIndex = 5;
+            this.chkRing.Text = "Name-Engraved Ring";
+            this.chkRing.UseVisualStyleBackColor = true;
+            this.chkRing.CheckedChanged += new System.EventHandler(this.chkRing_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.chkRing);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 77);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Character";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SoulMemory";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblMin);
+            this.groupBox2.Controls.Add(this.lblMax);
+            this.groupBox2.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 75);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Matches with";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 183);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "DS2 Soulmemory Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.CheckBox chkRing;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoulMemory_Calc
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<int, Tuple<int,int>> soulTiers;
+        int soulTier;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupDictionary();
+            txtInput.MaxLength = 9;
+            this.TopMost = true;
+        }
+
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            UpdateLabels();
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+            
+        }
+
+        private Tuple<int,int> getMatchingSoulLevels(int soulTier)
+        {
+            int minMatchTier, maxMatchTier;
+            int minMatchSM, maxMatchSM;
+            minMatchTier = chkRing.Checked ? soulTier - 6 : soulTier - 3;
+            if(minMatchTier <=0) { minMatchTier = 1; }
+            maxMatchTier = chkRing.Checked ? soulTier + 4 : soulTier + 1;
+            if(maxMatchTier > 44) { maxMatchTier = 44; }
+
+            soulTiers.TryGetValue(minMatchTier, out Tuple<int, int> resMin);
+            minMatchSM = resMin.Item1;
+            soulTiers.TryGetValue(maxMatchTier, out Tuple<int, int> resMax);
+            maxMatchSM = resMax.Item2;
+            return Tuple.Create(minMatchSM, maxMatchSM);
+
+        }
+        private int getTierFromSoulMemory(int sm)
+        {
+            for (int i = 1; i <= 44; i++)
+            {
+                soulTiers.TryGetValue(i, out Tuple<int, int> res);
+                if (sm >= res.Item1 && sm <= res.Item2)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        private void SetupDictionary()
+        {
+            soulTiers = new Dictionary<int, Tuple<int, int>>();
+
+            soulTiers.Add(1, Tuple.Create(0, 9999));
+            soulTiers.Add(2, Tuple.Create(10000, 19999));
+            soulTiers.Add(3, Tuple.Create(20000, 29999));
+            soulTiers.Add(4, Tuple.Create(30000, 39999));
+            soulTiers.Add(5, Tuple.Create(40000, 49999));
+            soulTiers.Add(6, Tuple.Create(50000, 69999));
+            soulTiers.Add(7, Tuple.Create(70000, 89999));
+            soulTiers.Add(8, Tuple.Create(90000, 109999));
+            soulTiers.Add(9, Tuple.Create(110000, 129999));
+            soulTiers.Add(10, Tuple.Create(130000, 149999));
+            soulTiers.Add(11, Tuple.Create(150000, 179999));
+            soulTiers.Add(12, Tuple.Create(180000, 209999));
+            soulTiers.Add(13, Tuple.Create(210000, 239999));
+            soulTiers.Add(14, Tuple.Create(240000, 269999));
+            soulTiers.Add(15, Tuple.Create(270000, 299999));
+            soulTiers.Add(16, Tuple.Create(300000, 349999));
+            soulTiers.Add(17, Tuple.Create(350000, 399999));
+            soulTiers.Add(18, Tuple.Create(400000, 449999));
+            soulTiers.Add(19, Tuple.Create(450000, 499999));
+            soulTiers.Add(20, Tuple.Create(500000, 599999));
+            soulTiers.Add(21, Tuple.Create(600000, 699999));
+            soulTiers.Add(22, Tuple.Create(700000, 799999));
+            soulTiers.Add(23, Tuple.Create(800000, 899999));
+            soulTiers.Add(24, Tuple.Create(900000, 999999));
+            soulTiers.Add(25, Tuple.Create(1000000, 1099999));
+            soulTiers.Add(26, Tuple.Create(1100000, 1199999));
+            soulTiers.Add(27, Tuple.Create(1200000, 1299999));
+            soulTiers.Add(28, Tuple.Create(1300000, 1399999));
+            soulTiers.Add(29, Tuple.Create(1400000, 1499999));
+            soulTiers.Add(30, Tuple.Create(1500000, 1749999));
+            soulTiers.Add(31, Tuple.Create(1750000, 1999999));
+            soulTiers.Add(32, Tuple.Create(2000000, 2249999));
+            soulTiers.Add(33, Tuple.Create(2250000, 2499999));
+            soulTiers.Add(34, Tuple.Create(2500000, 2749999));
+            soulTiers.Add(35, Tuple.Create(2750000, 2999999));
+            soulTiers.Add(36, Tuple.Create(3000000, 4999999));
+            soulTiers.Add(37, Tuple.Create(5000000, 6999999));
+            soulTiers.Add(38, Tuple.Create(7000000, 8999999));
+            soulTiers.Add(39, Tuple.Create(9000000, 11999999));
+            soulTiers.Add(40, Tuple.Create(12000000, 14999999));
+            soulTiers.Add(41, Tuple.Create(15000000, 19999999));
+            soulTiers.Add(42, Tuple.Create(20000000, 29999999));
+            soulTiers.Add(43, Tuple.Create(30000000, 44999999));
+            soulTiers.Add(44, Tuple.Create(45000000, 999999999));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void UpdateLabels()
+        {
+            if (txtInput.Text == "") { return; }
+
+            try
+            {
+                soulTier = getTierFromSoulMemory(Int32.Parse(txtInput.Text));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            if (soulTier != 0)
+            {
+                lblMin.Text = "Min : " + getMatchingSoulLevels(soulTier).Item1.ToString();
+                lblMax.Text = "Max : " + getMatchingSoulLevels(soulTier).Item2.ToString();
+            }
+        }
+        private void chkRing_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLabels();
+        }
+
+    }
+}
